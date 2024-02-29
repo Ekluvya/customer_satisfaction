11b22088faa03550005ff29a34b9689c800ff526
Yash Khare <47681673+Ekluvya@users.noreply.github.com>
2024-02-29 11:27:08 +0530

    Implemented ML Pipeline

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -1,13 +1,39 @@
 import logging
 import pandas as pd
+from src.data_cleaning import DataCleaning, DataDivideStrategy, DataPreProcessStrategy
+from typing_extensions import Annotated
+from typing import Tuple
 from zenml import step
 
 @step
-def clean_df(df: pd.DataFrame) -> None:
+def clean_df(df: pd.DataFrame) -> Tuple[
+    Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.Series, "y_test"],
+]:
     """
-    Cleaned the ingested data
+    Cleaned the ingested data and divides it into train and test
 
     Args:
         df: the ingested data
+    Returns:
+        X_train: Training data
+        X_test: Testing data
+        y_train: Training labels
+        y_test: Testing labels
     """
-    pass
+    try:
+        process_strategy = DataPreProcessStrategy()
+        data_cleaning = DataCleaning(df, process_strategy)
+        processed_data = data_cleaning.handle_data()
+
+        divide_strategy = DataDivideStrategy()
+        data_cleaning = DataCleaning(processed_data, divide_strategy)
+        X_train, X_test, y_train, y_test = data_cleaning.handle_data()
+        logging.info("Data cleaning completed!")
+        return X_train, X_test, y_train, y_test
+    
+    except Exception as e:
+        logging.error("Error in cleaning data: {}".format(e))
+        raise e
